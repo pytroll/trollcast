937866e59ea298312dc148c47ef56f13c1a89ad0
Martin Raspaud <martin.raspaud@smhi.se>
2012-11-14 15:44:59 +0100

    Update the doc.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['zmq', 'numpy']
+MOCK_MODULES = ['zmq', 'numpy', 'posttroll']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
     
