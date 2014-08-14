0e8894f2f07157b7a9d872f7f419ba5b91043ffe
Martin Raspaud <martin.raspaud@smhi.se>
2014-08-14 17:22:14 +0200

    Add support for kongsberg schedules.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- trollcast/schedules.py
+++ trollcast/schedules.py
@@ -23,10 +23,11 @@
 """Read schedule files.
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 formats = ["scisys"]
 satellites = ["NOAA 15", "NOAA 16", "NOAA 18", "NOAA 19"]
+
 
 def scisys(filename):
     """Read a scisys schedule
@@ -41,9 +42,32 @@ def scisys(filename):
                 break
             elts = line[16:].split()
             sat = line[:16].strip()
-            rec = elts[8]
-            if rec == "Y" and sat in satellites:
+            rec = elts[8] == "Y"
+            if rec and sat in satellites:
                 rise, fall = "".join(elts[1:3]), "".join(elts[3:5])
                 rise = datetime.strptime(rise, "%Y%m%d%H%M%S")
                 fall = datetime.strptime(fall, "%Y%m%d%H%M%S")
+                yield (rise, sat, fall)
+
+
+def kongsberg_metno(filename):
+    """Read a kongsberg schedule
+    """
+    with open(filename) as fp_:
+        while True:
+            line = fp_.readline()
+            if line.startswith(" No."):
+                continue
+
+            elts = line.split()
+            try:
+                sat = elts[2].replace("_", " ")
+            except IndexError:
+                break
+            rec = elts[13].startswith("Schedule")
+            if rec and sat in satellites:
+                rise = " ".join((elts[1], elts[5]))
+                rise = datetime.strptime(rise, "%Y%m%d %H:%M:%S")
+                mins, secs = elts[8].split(":")
+                fall = rise + timedelta(minutes=int(mins), seconds=int(secs))
                 yield (rise, sat, fall)
